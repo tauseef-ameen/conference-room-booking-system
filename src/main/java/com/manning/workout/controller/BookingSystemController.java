9ebaff1c4d0200a7ea2a451a0de0c9159f737a97
tauseef-ameen <tauseef.125002@gmail.com>
2025-03-02 12:38:12 +0100

    Adapted Controller

--- src/main/java/com/manning/workout/controller/BookingSystemController.java
+++ src/main/java/com/manning/workout/controller/BookingSystemController.java
@@ -1,25 +1,27 @@
 package com.manning.workout.controller;
 
+import com.manning.workout.exception.ServiceUnavailableException;
 import com.manning.workout.inventory.ConferenceRoom;
 import com.manning.workout.inventory.InventoryConferenceRoomClient;
 import com.manning.workout.reservation.RoomReservation;
 import com.manning.workout.reservation.RoomReservationRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class BookingSystemController {
 
     private final RoomReservationRepository reservationsRepository;
@@ -28,52 +30,51 @@ public class BookingSystemController {
     @Value("${room.state.service.url}")
     private String roomStateServiceUrl;
 
-    public BookingSystemController(RoomReservationRepository reservationsRepository, InventoryConferenceRoomClient inventoryClient) {
-        this.reservationsRepository = reservationsRepository;
-        this.inventoryClient = inventoryClient;
-    }
-
     @GetMapping("/available/{startDate}/{endDate}")
     public Collection<ConferenceRoom> availability(@PathVariable("startDate") LocalDateTime startDate,
                                                    @PathVariable("endDate") LocalDateTime endDate) {
 
         log.info("Getting available conference rooms for startDate {} and endDate {}", startDate, endDate);
-        // obtain all cars from inventory
+        // obtain all conf rooms from inventory
         List<ConferenceRoom> availableRooms = inventoryClient.allRooms();
-        // create a map from id to car
-        Map<Integer, ConferenceRoom> carsById = new LinkedHashMap<>();
-        for (ConferenceRoom room : availableRooms) {
-            carsById.put(room.getRoomId(), room);
-        }
+        // create a map from id to conf room
+        Map<Integer, ConferenceRoom> roomsById = availableRooms
+                .stream()
+                .collect(Collectors.toMap(ConferenceRoom::roomId, room -> room));
 
-        // get all current reservations
-        List<RoomReservation> reservations = reservationsRepository.findAll();
-        // for each reservation, remove the car from the map
-        for (RoomReservation reservation : reservations) {
-            if (reservation.isAvailable(startDate, endDate)) {
-                carsById.remove(reservation.roomId);
-            }
-        }
-        return carsById.values();
+        // get all current conf room Reservation List
+        List<RoomReservation> roomReservationList = reservationsRepository.findAll();
+        // for each reservation, remove the conf room from the map
+        roomReservationList.stream()
+                .filter(reservation -> reservation.isAvailable(startDate, endDate))
+                .forEach(reservation -> roomsById.remove(reservation.roomId));
+
+        log.info("Found {} conference rooms", roomsById.size());
+        return roomsById.values();
 
     }
 
     @PostMapping("/reserve")
-    public RoomReservation reserve(RoomReservation roomReservation) {
-        RoomReservation reservation = reservationsRepository.save(roomReservation);
-        String state = getRoomState(reservation.roomId);
+    public ResponseEntity<?> reserve(@RequestBody RoomReservation roomReservation) {
+        // Call Room state microservice to get real time state of room
+        String state = getRoomState(roomReservation.roomId);
         if (!"Available".equals(state)) {
-            log.info("Room is currently in state {} and cannot be booked", state);
+            log.error("Room is currently in state {} and cannot be booked", state);
+            return ResponseEntity.badRequest().body("Room is currently in state " +state+ "and cannot be booked");
         }
-        return reservation;
+        // save room reservation request and generate booking ID
+        RoomReservation reservation = reservationsRepository.save(roomReservation);
+        return ResponseEntity.ok(reservation);
     }
 
     private String getRoomState(int roomId) {
         String url = roomStateServiceUrl + "/state/" + roomId;
-        log.info("Getting room state url {}", url);
-        return restTemplate.getForObject(url, String.class);
-
+        try {
+            log.info("Getting room state url {}", url);
+            return restTemplate.getForObject(url, String.class);
+        } catch (RestClientException e) {
+            log.error("Service unavailable: {}", url, e);
+            throw new ServiceUnavailableException("Room state service is unavailable");
+        }
     }
-
-
 }
